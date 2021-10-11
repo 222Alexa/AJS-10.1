e1453da13eb28168af3f00a77c1717d05ddbae38
Alexandra Titova <auto-steklo@mail.ru>
2021-10-11 12:56:14 +0300

    first

---
+++ src/js/__tests__/gameSavingLoader.test.js
@@ -0,0 +1,10 @@
+import GameSavingLoader from "../GameSavingLoader";
+
+test("load() возвращает корректные данные", (done) => {
+  const result =
+    '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}';
+  GameSavingLoader.load().then((saving) => {
+    expect(saving).toEqual(result);
+    done();
+  });
+});
